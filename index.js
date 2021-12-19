c1bb6b9acdc783008158e66dac5c759de2b6a7cf
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-12-19 23:43:18 +0600

    port chaneg

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 
 const express = require('express')
 const app = express()
-const port = 8000
+//const port = 8000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(8000)
