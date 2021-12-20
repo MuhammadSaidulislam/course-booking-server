d60c7daa1e50e0008430bd7ca14bd5b37ea07fea
saidulislam <mislam171222@bscse.uiu.ac.bd>
2021-12-20 16:00:11 +0600

    mongoDB data recive

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const ObjectId = require("mongodb").ObjectId;
 
-const express = require('express')
-const app = express()
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 //const port = 8000
+const password = "hotel";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://hotel:hotel@cluster0.npcff.mongodb.net/Hotel_Booking?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("Hotel_Booking").collection("hotelBooking");
+  // data send to mongodb from ui
+  app.post('/addBooking', (req, res) => {
+    const newBooking = req.body;
+    collection.insertOne(newBooking)
+    .then(result => {
+      res.send(result.insertedCount > 0);
+      // res.redirect("/");
+    });
+    console.log(newBooking);
+  });
+});
 
-app.listen(8000)
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(8000);
